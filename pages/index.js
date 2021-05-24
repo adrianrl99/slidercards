5dd8629d2adf18ec68ac24bc95c486ef8848f2fd
adrianrl99 <61700204+adrianrl99@users.noreply.github.com>
2021-05-24 12:59:00 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
-import { Anchor, Box, Heading, Paragraph } from "grommet";
+import { Box } from "grommet";
+import { Github } from "grommet-icons";
 import CustomCard from "../src/components/CustomCard";
 import SliderCards from "../src/components/SliderCards";
 
 export default function Home() {
   return (
     <Box margin="large">
+      <Box flex={true} align="end" margin={"medium"}>
+        <a
+          href="https://github.com/adrianrl99/slidercards"
+          target="_blank"
+          title="Enlace a GitHub"
+        >
+          <Github />
+        </a>
+      </Box>
       <SliderCards title={"Carrusel"}>
         {Array(20)
           .fill()
